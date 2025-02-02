251b5c6adb9f6533927be9e224299b08c7c6d2ed
Alexey Koran <alexey.koran@gmail.com>
2025-02-03 02:20:14 +0400

    feat: `Table`, add `isLoading` logic, shimmer styles

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -5,30 +5,60 @@ import './Table.css';
 interface TableProps<T extends Record<keyof T, unknown>> {
   columns: (keyof T)[];
   data: T[];
+  isLoading: boolean;
 }
 
 export class Table<T extends Record<keyof T, unknown>> extends Component<
   TableProps<T>
 > {
   render() {
-    const { columns, data } = this.props;
+    const { columns, data, isLoading } = this.props;
+
+    if (isLoading) {
+      return (
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                {columns.map((column, index) => (
+                  <th key={index}>{String(column)}</th>
+                ))}
+              </tr>
+            </thead>
+
+            <tbody>
+              {Array(5)
+                .fill(0)
+                .map((_, index) => (
+                  <tr key={index} className="shimmer-row">
+                    {columns.map((_, colIndex) => (
+                      <td key={colIndex}>
+                        <div className="skeleton-cell"></div>
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
 
     return (
       <div className="table-container">
         <table>
           <thead>
             <tr>
-              {columns.map((column) => (
-                <th key={String(column)}>{String(column).toUpperCase()}</th>
+              {columns.map((column, index) => (
+                <th key={index}>{String(column).toUpperCase()}</th>
               ))}
             </tr>
           </thead>
-
           <tbody>
             {data.map((row, rowIndex) => (
               <tr key={rowIndex}>
-                {columns.map((column) => (
-                  <td key={String(column)}>{String(row[column])}</td>
+                {columns.map((column, colIndex) => (
+                  <td key={colIndex}>{String(row[column])}</td>
                 ))}
               </tr>
             ))}
