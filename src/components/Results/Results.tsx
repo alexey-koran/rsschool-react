2f9484f6991af16b84c48363cd6b56c539c2ab0b
Alexey Koran <alexey.koran@gmail.com>
2025-02-03 01:09:06 +0400

    feat: use `Table` in `Results`

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -2,28 +2,29 @@ import { Component } from 'react';
 
 import type { Planet } from '../../types/Planet';
 
+import { Table } from '../Table';
+
 interface Props {
   results: Planet[];
 }
+
+const columns: (keyof Planet)[] = [
+  'name',
+  'population',
+  'climate',
+  'terrain',
+  'diameter',
+  'gravity',
+];
 
 export class Results extends Component<Props> {
   render() {
     const { results } = this.props;
 
     return (
-      <div>
+      <div className="results">
         {results.length > 0 ? (
-          <ul>
-            {results.map((item, index) => (
-              <li key={index}>
-                <h3>{item.name}</h3>
-                <p>{item.population}</p>
-                <p>{item.climate}</p>
-                <p>{item.diameter}</p>
-                <p>{item.gravity}</p>
-              </li>
-            ))}
-          </ul>
+          <Table<Planet> columns={columns} data={results} />
         ) : (
           <div>No results.</div>
         )}
