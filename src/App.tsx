3697e5a36be2081d3f1be349bc29b7b643953c51
Alexey Koran <alexey.koran@gmail.com>
2025-02-02 22:37:20 +0400

    feat: implement base `App` component logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,68 @@
 import { Component } from 'react';
-import reactLogo from './assets/react.svg';
+
+import type { Planet } from './types/Planet';
+
+import { Search } from './components/Search';
+import { Results } from './components/Results';
 
 import './App.css';
 
-export default class App extends Component {
+interface State {
+  query: string;
+  results: Planet[];
+  loading: boolean;
+  error: Error | null;
+}
+
+export class App extends Component<object, State> {
+  constructor(props: object) {
+    super(props);
+
+    this.state = {
+      query: '',
+      results: [],
+      loading: false,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    const savedQuery = localStorage.getItem('searchQuery') ?? '';
+
+    this.setState({ query: savedQuery });
+  }
+
+  setSearchResults = (results: Planet[]) => {
+    this.setState({ results });
+  };
+
+  setLoading = (loading: boolean) => {
+    this.setState({ loading });
+  };
+
+  setError = (error: Error | null) => {
+    this.setState({ error });
+  };
+
   render() {
+    const { query, results, loading, error } = this.state;
+
     return (
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <Search
+          query={query}
+          setSearchResults={this.setSearchResults}
+          setLoading={this.setLoading}
+          setError={this.setError}
+        />
+
+        <div className="results-section">
+          {loading && <div>Loading...</div>}
+
+          {error && <div>{error.message}</div>}
+
+          <Results results={results} />
+        </div>
       </div>
     );
   }
