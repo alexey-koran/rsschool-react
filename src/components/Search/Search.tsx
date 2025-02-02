ae6c68c11aea5c4d37b4af5be4e7880b470745b5
Alexey Koran <alexey.koran@gmail.com>
2025-02-03 02:31:09 +0400

    feat: add `Search` styles

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -8,6 +8,8 @@ import { Button } from '../Button';
 import { handleFetch } from '../../utils/fetch';
 import { API_URL } from '../../constants';
 
+import './Search.css';
+
 interface Props {
   query: string;
   setSearchResults: (results: Planet[]) => void;
